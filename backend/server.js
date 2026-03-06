c9295ff3a4b5dca447ca5e4a00384a7d5defb211
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-03-06 22:38:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.use(helmet());
 // }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://tenant-frontend-zpux.onrender.com'],
   credentials: true
 }))
 
